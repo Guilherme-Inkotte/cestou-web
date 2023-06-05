2b071c120c4aee5e1665e8fe73e9af2573b8e95a
Guilherme Inkotte <guilherme.inkotte@dentsu.com>
2023-06-05 20:03:29 -0300

    Created list components

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
+myMatchesButton = document.getElementById("my-matches");
+publicMatchesButton = document.getElementById("public-matches");
+courtsButton = document.getElementById("courts");
 
+myMatchesButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  myMatchesButton.classList.add("selected");
+  publicMatchesButton.classList.remove("selected");
+  courtsButton.classList.remove("selected");
+});
+
+publicMatchesButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  myMatchesButton.classList.remove("selected");
+  publicMatchesButton.classList.add("selected");
+  courtsButton.classList.remove("selected");
+});
+
+courtsButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  myMatchesButton.classList.remove("selected");
+  publicMatchesButton.classList.remove("selected");
+  courtsButton.classList.add("selected");
+});
