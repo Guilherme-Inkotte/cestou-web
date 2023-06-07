3d73a9005f5870e7627e0d14636ad9031fafa4ab
Guilherme Inkotte <gui.inkotte@hotmail.com>
2023-06-06 22:37:26 -0300

    Finished Home list components

--- js/home.js
+++ js/home.js
@@ -1,25 +1,37 @@
-myMatchesButton = document.getElementById("my-matches");
-publicMatchesButton = document.getElementById("public-matches");
-courtsButton = document.getElementById("courts");
+myMatchesButton = document.getElementById("my-matches-button");
+publicMatchesButton = document.getElementById("public-matches-button");
+courtsButton = document.getElementById("courts-button");
+myMatches = document.getElementById("my-matches");
+publicMatches = document.getElementById("public-matches");
+courts = document.getElementById("courts");
 
 myMatchesButton.addEventListener("click", (e) => {
   e.preventDefault();
   myMatchesButton.classList.add("selected");
+  myMatches.classList.remove("hidden");
   publicMatchesButton.classList.remove("selected");
+  publicMatches.classList.add("hidden");
   courtsButton.classList.remove("selected");
+  courts.classList.add("hidden");
 });
 
 publicMatchesButton.addEventListener("click", (e) => {
   e.preventDefault();
   myMatchesButton.classList.remove("selected");
+  myMatches.classList.add("hidden");
   publicMatchesButton.classList.add("selected");
+  publicMatches.classList.remove("hidden");
   courtsButton.classList.remove("selected");
+  courts.classList.add("hidden");
 });
 
 courtsButton.addEventListener("click", (e) => {
   e.preventDefault();
   myMatchesButton.classList.remove("selected");
+  myMatches.classList.add("hidden");
   publicMatchesButton.classList.remove("selected");
+  publicMatches.classList.add("hidden");
   courtsButton.classList.add("selected");
+  courts.classList.remove("hidden");
 });
 
